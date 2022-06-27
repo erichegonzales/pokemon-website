9f45b24bf7ea0d493720f2b9976dce4f86f23463
Eriche Gonzales <erichelouise@gmail.com>
2022-06-27 17:46:21 -0400

    Added styles

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
 const pokemon = ['Charmander', 'Bulbosaur', 'Squirtle']
+const pokemonIDs = ['001', '004', '007']
+
+// DOM = Document Object Model
+// loop over every ID
+// create an HTML Element
+// set the HTML element values
+// append HTML element to the DOM
+
+pokemonIDs.map((id) => {
+    let imgUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id}.png`
+    console.log('img url', imgUrl)
+    let div = document.createElement('div') // adding a div to house the image element
+    div.setAttribute('class', 'pokemon-card') // setting the class of the div
+    let img = document.createElement('img') // creating the img element itself
+    img.src = imgUrl
+    div.append(img) // append the img element to the div
+    document.body.append(div)
+})
 
