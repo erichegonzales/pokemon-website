ec9d844d32e703c11bce5e3c42f621680177a3a4
Eriche Gonzales <erichelouise@gmail.com>
2022-06-28 13:31:15 -0400

    Created roster container

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
-const pokemon = ['Charmander', 'Bulbosaur', 'Squirtle']
-const pokemonIDs = ['001', '004', '007']
+const pokemon = [
+    {name: "Bulbasaur", id: "001"},
+    {name: "Charmander", id: "004"},
+    {name: "Squirtle", id: "007"},
+]
+
 const containerDiv = document.querySelector('#container')
+const newBtn = document.querySelector('#new-pokemon-btn')
+const rosterDiv = document.querySelector('#roster')
+
+newBtn.addEventListener('click', () => {
+    let num = prompt('ENTER A POKEMON NUMBER')
+    console.log('Number entered: ', num)
+})
+
+
 
 // DOM = Document Object Model
 // loop over every ID
@@ -8,15 +21,31 @@ const containerDiv = document.querySelector('#container')
 // set the HTML element values
 // append HTML element to the DOM
 
-pokemonIDs.map((id) => {
-    let imgUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id}.png`
-    console.log('img url', imgUrl)
+pokemon.map((element, index) => {
+    console.log(element)
+    let imgUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${element.id}.png`
     let div = document.createElement('div') // adding a div to house the image element
+    let h3 = document.createElement('h3')
+    h3.innerText = element.name
     div.setAttribute('class', 'pokemon-card') // setting the class of the div
     let img = document.createElement('img') // creating the img element itself
+
+    // audio element existing before it is clicked on / waiting to be clicked on
+    let audioUrl = `https://play.pokemonshowdown.com/audio/cries/${element.name.toLowerCase()}.mp3`
+    let audio = document.createElement('audio')
+    let source = document.createElement('source')
+    source.setAttribute('src', audioUrl) // use this source
+    source.setAttribute('type', 'audio/mpeg') // type of file
+    audio.append(source) // put source in the audio
+    div.addEventListener('click', () => {
+        console.log('audio', audioUrl)
+        audio.play();
+    })
+
     img.src = imgUrl
-    div.append(img) // append the img element to the div
-    document.body.append(div)
+    div.append(img, h3, audio) // append the elements to the div
     containerDiv.append(div)
 })
+
+
 
