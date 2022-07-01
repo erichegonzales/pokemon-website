a20eb0f0000cb38d48e9fd372ea06d53c3f485c0
Eriche Gonzales <erichelouise@gmail.com>
2022-07-01 15:26:11 -0400

    Added alert when roster is full

--- main.js
+++ main.js
@@ -28,57 +28,57 @@ const pokemon = [
 // empty array for the roster
 const userRoster = []
 
-const containerDiv = document.querySelector('#container') // storing the roster container with <button> and <div>
-const newBtn = document.querySelector('#new-pokemon-btn') // storing the button for adding new pokemong
+const containerDiv = document.querySelector('#container') // storing the roster with <button> and <div>
+const newBtn = document.querySelector('#new-pokemon-btn') // storing the button for adding new pokemon
 const rosterDiv = document.querySelector('#roster') // storing the roster <div> element
 
 // button for new pokemon
-// event that occurs when button is clicked
 newBtn.addEventListener('click', async () => {
+    // send alert when roster is full
     if (userRoster.length === 6) {
         return alert('ROSTER IS FULL')
     }
     
+    // creates an input for user to enter a pokemon number
     let num = prompt('ENTER A POKEMON NUMBER') // alert on website that tells user to enter a pokemon number 
 
-    // adds leading zeros for imgUrl
+    // adds leading zeros for imgUrl to work
     if (num.length === 2) numZeros = "0" + num
     else if (num.length === 1) numZeros = "00" + num
     else numZeros = num;
 
-    // remove leading zeros for dataUrl
+    // remove leading zeros for dataUrl to work
     num = parseInt(num, 10)
-    console.log('this is', num)
 
     console.log('Number entered: ', numZeros) // prints the number entered to DOM
-    let imgUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${numZeros}.png` // URL to images of pokemon
-    let dataUrl = `https://pokeapi.co/api/v2/pokemon/${num}` // URL to the data to the pokemon
-    // Fetch API accesses resources across the network. 
-    // make HTTP requests, download, and upload files
-    let req = await fetch(dataUrl)
-    let res = await req.json() // reads the request body and returns it
-    let name = res.forms[0].name
+    let imgUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${numZeros}.png` // URL for pokemon images
+    let dataUrl = `https://pokeapi.co/api/v2/pokemon/${num}` // URL for pokemon data to access name
+    // fetch API accesses resources across the network, which makes HTTP requests, download, and upload files
+    let req = await fetch(dataUrl) // gets data from URL
+    let res = await req.json() // read the request body and returns it
+    let name = res.forms[0].name // access the first element of the pokemon object, which is name
 
-    let audioUrl = `https://play.pokemonshowdown.com/audio/cries/${name.toLowerCase()}.mp3`
-    let audio = document.createElement('audio')
-    let source = document.createElement('source')
-    source.setAttribute('src', audioUrl) // use this source
-    source.setAttribute('type', 'audio/mpeg') // type of file
-    audio.append(source) // put source in the audio
+    let audioUrl = `https://play.pokemonshowdown.com/audio/cries/${name.toLowerCase()}.mp3` // URL for pokemon audio
+    let audio = document.createElement('audio') // create element for audio
+    let source = document.createElement('source') // create element for audio source
+    source.setAttribute('src', audioUrl) // attaches source to audio URL
+    source.setAttribute('type', 'audio/mpeg') // attaches type of source
+    audio.append(source) // appends the source to the audio element
 
-    let h3 = document.createElement('h3')
-    h3.innerText = name
-    let img = document.createElement('img')
-    img.setAttribute("src", imgUrl)
-    img.setAttribute('class', 'roster-img')
-    let position = document.querySelector(`#pokemon-${userRoster.length + 1}`)
-    position.append(img, h3, audio)
-    position.addEventListener('click', () => {
+    let h3 = document.createElement('h3') // create <h3> element
+    h3.innerText = name // adds the name to the <h3> element
+    let img = document.createElement('img') // create element for the image
+    img.setAttribute("src", imgUrl) // links the image to the URL source
+    img.setAttribute('class', 'roster-img') // add class attribute to image
+    let position = document.querySelector(`#pokemon-${userRoster.length + 1}`) // selects all the pokemon elements
+    position.append(img, h3, audio) // appends these elements to the pokemon roster
+    position.addEventListener('click', () => { // plays the pokemon audio when you click
         audio.play()
     })
-    userRoster.push(num)
+    userRoster.push(num) // adds the pokemon to the roster array
 })
 
+// function to create pokemon
 const createPokemon = (pokemonName, imgUrl) => {
     let div = document.createElement('div') // adding a div to house the image element
     let h3 = document.createElement('h3')
@@ -97,12 +97,12 @@ const createPokemon = (pokemonName, imgUrl) => {
         div.classList.add('animate__animated')
         div.classList.add('animate__shakeX')
         setTimeout(() => {
-            div.classList.add('animate__shakeX') // but always going to be moving the card
+            div.classList.add('animate__shakeX')
         }, 1300)
         audio.play();
     })
     img.src = imgUrl
-    div.append(img, h3, audio) // append the elements to the div
+    div.append(img, h3, audio) // append the elements to the <div> where pokemon is stored
     return div;
 }
 
